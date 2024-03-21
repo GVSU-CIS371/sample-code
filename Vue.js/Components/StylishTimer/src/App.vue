4f3b8e1f5039271966163090e46add6abb69433f
yong-zhuang <jazz14jazz@gmail.com>
2024-03-21 02:17:04 +0000

    add stylish timer sample code

---
+++ Vue.js/Components/StylishTimer/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Timer from "./components/Timer.vue";
+</script>
+
+<template>
+  <h1>Hello Stylish Timers</h1>
+
+  <div id="timers">
+    <Timer :updateInterval="100" :faceColor="`red`"></Timer>
+    <Timer :startLabel="`Go`"></Timer>
+  </div>
+</template>
+<style>
+#timers {
+  display: flex;
+  justify-content: space-evenly;
+}
+</style>
