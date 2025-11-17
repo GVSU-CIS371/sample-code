91a7297f9f5fdb24b4c2a10872169e7ac82c758e
yong-zhuang <jazz14jazz@gmail.com>
2025-11-17 18:55:49 +0000

    firebase config remove

--- Firebase/Authentication/SigninSignup/src/main.ts
+++ Firebase/Authentication/SigninSignup/src/main.ts
@@ -3,25 +3,15 @@ import App from "./App.vue";
 import router from "./router";
 import { initializeApp } from "firebase/app";
 
-// const firebaseConfig = {
-//   // COPY this from your Firebase Console
-//   apiKey: "your-api-key-goes-here",
-//   authDomain: "your-project-name-here.firebaseapp.com",
-//   databaseURL: "https://your-project-name-here.firebaseio.com",
-//   projectId: "your-project-name-here",
-//   storageBucket: "your-project-name.appspot.com",
-//   messagingSenderId: "xxxxxxxx",
-// };
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBF-CRMH-LsqbrVSB5ZF_eBUYU8P-kdAIE",
-  authDomain: "cis371-demo-770dc.firebaseapp.com",
-  projectId: "cis371-demo-770dc",
-  storageBucket: "cis371-demo-770dc.appspot.com",
-  messagingSenderId: "988644841148",
-  appId: "1:988644841148:web:3b246d80be5da0bccb62b9",
-  measurementId: "G-X2KWMBZYFB",
+  // COPY this from your Firebase Console
+  apiKey: "your-api-key-goes-here",
+  authDomain: "your-project-name-here.firebaseapp.com",
+  databaseURL: "https://your-project-name-here.firebaseio.com",
+  projectId: "your-project-name-here",
+  storageBucket: "your-project-name.appspot.com",
+  messagingSenderId: "xxxxxxxx",
 };
+
 initializeApp(firebaseConfig);
 createApp(App).use(router).mount("#app");
